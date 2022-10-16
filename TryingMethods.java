9bec4249536434f68ecaf9fd3e713615fbc07e5d
Nyawira Gitaka <nyawira.gitaka@gmail.com>
2022-10-16 17:26:25 +0300

    Switch case for days of the week

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,6 +1,36 @@
 public class TryingMethods {
+    // This class allows for the testing of various methods.
+    // acess modifier method type return type method name
+    public String dayOfWeek(int day) {
 
-    public static void name() {
-        
+        String dayofweek = "";
+        switch (day) {
+            case 1:
+                dayofweek = "Sunday";
+                break;
+            case 2:
+                dayofweek = "Monday";
+                break;
+            case 3:
+                dayofweek = "Tuesday";
+                break;
+            case 4:
+                dayofweek = "Wednesday";
+                break;
+            case 5:
+                dayofweek = "Thursday";
+                break;
+            case 6:
+                dayofweek = "Friday";
+                break;
+            case 7:
+                dayofweek = "Monday";
+                break;
+            default:
+                dayofweek = "Invalid entry";
+                break;
+        }
+        return dayofweek;
+
     }
 }
